a61fb91da858fd256ba6708a1657a7d5ab87658d
limhuot <lim.hout9988@gmail.com>
2025-09-24 15:15:57 +0700

    update dashboard ui

--- resources/js/Pages/Dashboards/Dashboard.jsx
+++ resources/js/Pages/Dashboards/Dashboard.jsx
@@ -19,12 +19,11 @@ const Dashboard = () => {
                                     <div className="col-lg-3 col-6">
                                         <div className="small-box bg-info">
                                             <div className="inner">
-                                                <h3>150</h3>
-
-                                                <p>New Orders</p>
+                                                <h3>120</h3>
+                                                <p>Total Homophones</p>
                                             </div>
                                             <div className="icon">
-                                                <i className="ion ion-bag"></i>
+                                                <i className="fas fa-volume-up"></i>
                                             </div>
                                             <a href="#" className="small-box-footer">More info <i className="fas fa-arrow-circle-right"></i></a>
                                         </div>
@@ -32,12 +31,11 @@ const Dashboard = () => {
                                     <div className="col-lg-3 col-6">
                                         <div className="small-box bg-success">
                                             <div className="inner">
-                                                <h3>53<sup style={{ fontSize: '20px' }}>%</sup></h3>
-
-                                                <p>Bounce Rate</p>
+                                                <h3>15</h3>
+                                                <p>Quizzes Created</p>
                                             </div>
                                             <div className="icon">
-                                                <i className="ion ion-stats-bars"></i>
+                                                <i className="fas fa-question-circle"></i>
                                             </div>
                                             <a href="#" className="small-box-footer">More info <i className="fas fa-arrow-circle-right"></i></a>
                                         </div>
@@ -46,11 +44,10 @@ const Dashboard = () => {
                                         <div className="small-box bg-warning">
                                             <div className="inner">
                                                 <h3>44</h3>
-
-                                                <p>User Registrations</p>
+                                                <p>Students Registered</p>
                                             </div>
                                             <div className="icon">
-                                                <i className="ion ion-person-add"></i>
+                                                <i className="fas fa-user-graduate"></i>
                                             </div>
                                             <a href="#" className="small-box-footer">More info <i className="fas fa-arrow-circle-right"></i></a>
                                         </div>
@@ -58,14 +55,28 @@ const Dashboard = () => {
                                     <div className="col-lg-3 col-6">
                                         <div className="small-box bg-danger">
                                             <div className="inner">
-                                                <h3>65</h3>
-
-                                                <p>Unique Visitors</p>
+                                                <h3>87</h3>
+                                                <p>Practice Sessions</p>
                                             </div>
                                             <div className="icon">
-                                                <i className="ion ion-pie-graph"></i>
+                                                <i className="fas fa-chalkboard-teacher"></i>
                                             </div>
                                             <a href="#" className="small-box-footer">More info <i className="fas fa-arrow-circle-right"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                {/* Graph/Chart Placeholder */}
+                                <div className="row mt-4">
+                                    <div className="col-12">
+                                        <div className="card">
+                                            <div className="card-header">
+                                                <h5 className="card-title mb-0">Homophone Activity Overview (Graph Placeholder)</h5>
+                                            </div>
+                                            <div className="card-body" style={{ minHeight: '250px', display: 'flex', alignItems: 'center', justifyContent: 'center', background: '#f8f9fa' }}>
+                                                <span style={{ color: '#aaa', fontSize: '1.2rem' }}>
+                                                    [Graph will be displayed here]
+                                                </span>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
